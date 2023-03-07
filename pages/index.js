fe643018705f1511c041ed51ed867a7638a9d4a5
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-03-07 18:26:34 +0200

    adding initial pages

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+const HomePage = () => {
+  return (
+    <Fragment>
+      <Hero />
+      <FreaturedPosts />
+    </Fragment>
+  );
+};
+
+export default HomePage;
