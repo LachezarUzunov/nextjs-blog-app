0cbaa208df97f9366427d16c56a42c6b31d91854
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-03-12 06:22:43 +0200

    Adding posts grid and item components

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,5 +1,13 @@
+import PostsGrid from "../posts/posts-grid";
 import classes from "./featured-posts.module.css";
 
-const FeaturedPosts = () => {};
+const FeaturedPosts = () => {
+  return (
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+      <PostsGrid />
+    </section>
+  );
+};
 
 export default FeaturedPosts;
