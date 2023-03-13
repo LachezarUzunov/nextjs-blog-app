609979b9b4bab63ac3957bd5d1002d5eddb590ac
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-03-13 17:27:23 +0200

    fetching all and featured posts and displaying

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,25 @@
 import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
+import { getFeaturedPosts } from "../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "the-role-of-the-father",
-    title: "The Role of the Father in the Family",
-    image: "the-role-of-the-father.jpg",
-    excerpt:
-      "In the past, fathers were typically seen as the primary breadwinners and providers, while mothers were responsible for the care and nurturing of children. However, this traditional view of fatherhood has evolved over time...",
-    date: "2023-03-12",
-  },
-  {
-    slug: "the-role-of-the-father2",
-    title: "The Role of the Father in the Family 2",
-    image: "the-role-of-the-father.jpg",
-    excerpt:
-      "In the past, fathers were typically seen as the primary breadwinners and providers, while mothers were responsible for the care and nurturing of children. However, this traditional view of fatherhood has evolved over time...",
-    date: "2023-03-12",
-  },
-];
-
-const HomePage = () => {
+const HomePage = (props) => {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={props.posts} />
     </Fragment>
   );
 };
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
+}
 
 export default HomePage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,26 +1,18 @@
 import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "../../lib/posts-util";
 
-const DUMMY_POSTS = [
-  {
-    slug: "the-role-of-the-father",
-    title: "The Role of the Father in the Family",
-    image: "the-role-of-the-father.jpg",
-    excerpt:
-      "In the past, fathers were typically seen as the primary breadwinners and providers, while mothers were responsible for the care and nurturing of children. However, this traditional view of fatherhood has evolved over time...",
-    date: "2023-03-12",
-  },
-  {
-    slug: "the-role-of-the-father2",
-    title: "The Role of the Father in the Family 2",
-    image: "the-role-of-the-father.jpg",
-    excerpt:
-      "In the past, fathers were typically seen as the primary breadwinners and providers, while mothers were responsible for the care and nurturing of children. However, this traditional view of fatherhood has evolved over time...",
-    date: "2023-03-12",
-  },
-];
-
-const AllPostsPage = () => {
-  return <AllPosts posts={DUMMY_POSTS} />;
+const AllPostsPage = (props) => {
+  return <AllPosts posts={props.posts} />;
 };
+
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
+}
 
 export default AllPostsPage;
